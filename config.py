fe287dff1832af1d5ee446588403016685547d03
SHAURYA_X_TEAM <100112352+shauryateam@users.noreply.github.com>
2023-09-17 07:36:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,8 +29,8 @@ AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 
 
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Life_Codes")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Life_Codes")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/SXK_143")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/SXK_143")
 
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
 SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
@@ -38,7 +38,7 @@ SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO","https://github.com/JustAyu/Aest")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO","https://github.com/shauryateam/Newmusic")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)  # Fill this variable if your upstream repository is private
 
